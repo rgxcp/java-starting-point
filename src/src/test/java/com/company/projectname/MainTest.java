c8987ea8a39985e8edb13e9a23a54aec5d0eb1c5
Rommy Gustiawan <rgcppp@gmail.com>
2021-09-21 14:20:10 +0700

    Update test file template format
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- src/src/test/java/com/company/projectname/MainTest.java
+++ src/src/test/java/com/company/projectname/MainTest.java
@@ -2,11 +2,12 @@ package com.company.projectname;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
     @Test
-    void methodNameTest(){
+    void methodNameTest() {
         // Your test logic here
+	// assertEquals(x, y)
     }
 }
